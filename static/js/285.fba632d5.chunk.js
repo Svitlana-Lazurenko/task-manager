70256c14c4d66aa120c6bbfc4a41a2b341dc5e21
Svitlana-Lazurenko <115888343+Svitlana-Lazurenko@users.noreply.github.com>
2023-09-15 18:04:17 +0000

    Deploying to gh-pages from @ Svitlana-Lazurenko/task-manager-frontend@69c35e6f563b0d5c6e51b3eb6a9206f76485d402 🚀

--- static/js/285.fba632d5.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[285],{285:function(){}}]);
--- static/js/38.bebb3edf.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[38],{38:function(){}}]);
