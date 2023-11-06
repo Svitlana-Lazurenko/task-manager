10625c0863b971b775aa1e3016051bf244c918f1
Svitlana Lazurenko <115888343+Svitlana-Lazurenko@users.noreply.github.com>
2023-11-06 16:39:57 +0200

    Change name of repository

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/task-manager-frontend">
+        <BrowserRouter basename="/task-manager">
           <App />
         </BrowserRouter>
       </PersistGate>
